4bcda2f5f2f88811ae5f6f482b3d50860f12eee6
Josip1123 <jopetk@hotmail.com>
2024-10-29 18:17:23 +0100

    added dark theme logic

--- src/components/Header/DarkmodeToggle.jsx
+++ src/components/Header/DarkmodeToggle.jsx
@@ -1,4 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react"
+
+
 function DarkmodeToggle() {
+  const localTheme = localStorage.getItem("theme") === "true"
+  const [isDarkMode, setDarkMode] = useState(localTheme)
+
+  if (isDarkMode) {
+    document.body.classList.add("dark-mode")
+  } else {
+    document.body.classList.remove("dark-mode")
+  }
+
+
+  const toggleDarkmode = () => {
+
+    setDarkMode((prevMode) => {
+
+      const updatedMode = !prevMode;
+
+      if (updatedMode) {
+        document.body.classList.add("dark-mode")
+      } else {
+        document.body.classList.remove("dark-mode")
+      }
+
+
+      localStorage.setItem("theme", `${updatedMode}`)
+
+      return updatedMode
+    });
+  };
+
   return (
     <label className="switch">
       <span className="darkmode-text">Dark Mode</span>
@@ -6,6 +39,8 @@ function DarkmodeToggle() {
         className="darkmode-toggle"
         type="checkbox"
         aria-label="toggle dark mode"
+        onChange={toggleDarkmode}
+        checked={isDarkMode}
       />
       <span className="slider round"></span>
     </label>
